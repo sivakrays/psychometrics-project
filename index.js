6c7cd0b059e4bde9cfefb0ddca161cab42d9b5c5
sivakrays <141822042+sivakrays@users.noreply.github.com>
2025-01-21 13:21:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json({ extends: false }));
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://psychometrics.onrender.com"],
   })
 );
 
